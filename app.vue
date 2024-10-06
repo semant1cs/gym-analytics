ac809ce3e06721c545e0a26a4b93c36d08433eb4
Petrov Mikhail <59141793+semant1cs@users.noreply.github.com>
2024-10-06 19:45:16 +0500

    вход в админку и красота на проекте

---
+++ app.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<script lang="ts" setup>
+useHead({ 
+  htmlAttrs: {
+    lang: 'ru-RU'
+  }
+})
+</script>
+
---
+++ components/admin/layouts/empty.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-layout class="empty-panel-layout">
+    <q-page-container>
+      <slot />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<style lang="scss">
+.empty-panel-layout {
+  @import '@/components/admin/css/common.scss';
+}
+</style>
