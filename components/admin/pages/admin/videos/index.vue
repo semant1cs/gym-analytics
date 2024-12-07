28dd3a071896ca6de9abed409ab7128385ccfc04
Петров Михаил Дмитриевич <m.d.petrov@urfu.me>
2024-12-07 22:12:28 +0500

    Added the ability to upload videos. Created video validation by max size and format

--- components/admin/pages/admin/videos/index.vue
+++ components/admin/pages/admin/videos/index.vue
@@ -1,73 +1,77 @@
 <template>
   <q-page class="w100">
     <q-table
-      v-model:selected="selected"
-      flat
-      bordered
-      title="Видео"
-      row-key="pageId"
-      hide-pagination
-      no-data-label="Данные отсутствуют"
-      loading-label="Загрузка..."
-      selection="multiple"
-      :selected-rows-label="(numberOfRows: number) => `Выбрано ${numberOfRows} шт.`"
-      :rows="data.rows"
-      :columns="columns"
-      :rows-per-page-options="[10]"
-      :loading="componentLoading"
+        v-model:selected="selected"
+        flat
+        bordered
+        title="Видео"
+        row-key="pageId"
+        hide-pagination
+        no-data-label="Данные отсутствуют"
+        loading-label="Загрузка..."
+        selection="multiple"
+        :selected-rows-label="(numberOfRows: number) => `Выбрано ${numberOfRows} шт.`"
+        :rows="data.rows"
+        :columns="columns"
+        :rows-per-page-options="[10]"
+        :loading="componentLoading"
     >
       <template
-        #top-right
+          #top-right
       >
         <q-input
-          v-model.trim="filterData.search"
-          class="mr-6 gt-sm filter-input"
-          dense
-          outlined
-          label="Поиск"
-          color="panel-primary"
+            v-model.trim="filterData.search"
+            class="mr-6 gt-sm filter-input"
+            dense
+            outlined
+            label="Поиск"
+            color="panel-primary"
         />
 
         <q-btn
-          class="mr-4"
-          round
-          flat
-          icon="add"
-          @click="isShowUploadModal = true"
+            class="mr-4"
+            round
+            flat
+            icon="add"
+            @click="isShowUploadModal = true"
         />
         <q-dialog v-model="isShowUploadModal">
           <q-card class="q-pa-md tw-flex tw-flex-col tw-gap-4 tw-justify-start tw-items-start">
             <q-input
-              v-model="videoName"
-              label="Название видео"
-              filled
+                v-model="videoName"
+                label="Название видео"
+                filled
             />
             <q-uploader
-              url="https://localhost:8083/api/v1/videos"
-              :headers="[{name: 'token', value: `Bearer ${bearerToken}` }]"
-             />
+                :headers="[{name: 'token', value: `Bearer ${bearerToken}` }]"
+                :disable="!videoFile"
+                @added="handleFileChange"
+                ref="uploader"
+                label="Выберите видео"
+            />
             <q-btn
-              label="Сохранить"
-              filled
-              color="primary"
-              class="tw-self-end"
-              @click="uploadVideo"
+                label="Сохранить"
+                filled
+                color="primary"
+                class="tw-self-end"
+                :loading="componentLoading"
+                @click="uploadVideo"
             />
           </q-card>
         </q-dialog>
-    <!-- Дообавить загрузку видео -->
+        <!-- Дообавить загрузку видео -->
         <q-btn
-          round
-          flat
-          icon="more_vert"
-          :loading="componentLoading"
+            round
+            flat
+            icon="more_vert"
+            :loading="componentLoading"
         >
           <q-menu>
             <q-list style="min-width: 135px">
               <q-item
-                v-close-popup
-                clickable
-                @click="deleteDialog = true"
+                  v-close-popup
+                  clickable
+                  @click="deleteDialog = true"
               >
                 <q-item-section>Удалить</q-item-section>
               </q-item>
@@ -79,18 +83,18 @@
 
     <div class="row justify-center mt-16">
       <q-pagination
-        v-model="pagination.page"
-        color="grey-8"
-        size="sm"
-        boundary-numbers
-        :max="pagination.max"
-        :max-pages="6"
-        @update:model-value="updatePagination"
+          v-model="pagination.page"
+          color="grey-8"
+          size="sm"
+          boundary-numbers
+          :max="pagination.max"
+          :max-pages="6"
+          @update:model-value="updatePagination"
       />
     </div>
 
     <q-dialog
-      v-model="deleteDialog"
+        v-model="deleteDialog"
     >
       <q-card>
         <q-card-section>
@@ -105,17 +109,17 @@
 
         <q-card-actions align="right">
           <q-btn
-            v-close-popup
-            flat
-            label="Отмена"
-            color="panel-primary"
+              v-close-popup
+              flat
+              label="Отмена"
+              color="panel-primary"
           />
           <q-btn
-            v-close-popup
-            flat
-            label="Удалить"
-            color="negative"
-            @click="doDelete"
+              v-close-popup
+              flat
+              label="Удалить"
+              color="negative"
+              @click="doDelete"
           />
         </q-card-actions>
       </q-card>
@@ -200,6 +204,7 @@ const pagination = ref({
   page: 1,
   max: 0
 })
+const videoFile = ref<File | null>(null)
 
 const updatePagination = () => {
   loadData()
@@ -208,6 +213,14 @@ const updatePagination = () => {
 onMounted(() => {
   loadData()
 })
+
+/** Обработчик при выборе видео в инпуте */
+const handleFileChange = (files: readonly any[]) => {
+  /** Мы предполагаем, что пользователь выберет только 1 видеофайл */
+  if (files && files.length > 0) {
+    videoFile.value = files[0]
+  }
+}
 
 const loadData = async () => {
   componentLoading.value = true
@@ -224,18 +237,18 @@ const loadData = async () => {
   await apiFetch(url, {
     method: 'get'
   })
-    .then((result: any) => {
-      pagination.value.max = Math.ceil(result.count / 10)
-      data.value = result
-    })
-    .catch((err) => {
-      notify({
-        color: 'negative',
-        textColor: 'white',
-        icon: 'warning',
-        message: err.message || 'Упс, произошла ошибка'
+      .then((result: any) => {
+        pagination.value.max = Math.ceil(result.count / 10)
+        data.value = result
       })
-    })
+      .catch((err) => {
+        notify({
+          color: 'negative',
+          textColor: 'white',
+          icon: 'warning',
+          message: err.message || 'Упс, произошла ошибка'
+        })
+      })
 
   componentLoading.value = false
 }
@@ -257,31 +270,81 @@ const doDelete = async () => {
       'Access-Control-Allow-Credentials': true
     }
   })
-    .then((result: any) => {
-      notify({
-        color: 'positive',
-        textColor: 'white',
-        icon: 'done',
-        message: result.message || 'Успех'
-      })
+      .then((result: any) => {
+        notify({
+          color: 'positive',
+          textColor: 'white',
+          icon: 'done',
+          message: result.message || 'Успех'
+        })
 
-      loadData()
-      selected.value = []
-    })
-    .catch((err) => {
-      notify({
-        color: 'negative',
-        textColor: 'white',
-        icon: 'warning',
-        message: err.message || 'Упс, произошла ошибка'
+        loadData()
+        selected.value = []
       })
-    })
+      .catch((err) => {
+        notify({
+          color: 'negative',
+          textColor: 'white',
+          icon: 'warning',
+          message: err.message || 'Упс, произошла ошибка'
+        })
+      })
 
   componentLoading.value = false
 }
 
-const uploadVideo = () => {
+const showNotify = (message: string = 'Ошибка', icon?: string) => {
+  return notify({
+    color: 'negative',
+    textColor: 'white',
+    icon: icon ? 'warning' : icon,
+    message: message
+  })
+}
 
+/** Метод валидации и отправки видеофайла на сервер */
+const uploadVideo = async () => {
+  if (!videoFile.value) {
+    showNotify('Пожалуйста, выберите файл для загрузки!');
+
+    return;
+  }
+
+  /** Проверка формата файла */
+  if (videoFile.value.type !== 'video/mp4') {
+    showNotify('Файл должен быть в формате .mp4');
+
+    return;
+  }
+
+  /** Проверка размера файла (максимум 300 MB) */
+  if (videoFile.value.size > 300 * 1024 * 1024) {
+    showNotify('Файл не должен превышать 300 MB');
+
+    return;
+  }
+
+  componentLoading.value = true;
+
+  const formData = new FormData();
+  formData.append('video', videoFile.value);
+  formData.append('name', videoName.value);
+
+  await apiFetch('/api/v1/videos', {
+    method: 'POST',
+    body: formData,
+    headers: {
+      'token': `Bearer ${bearerToken}`
+    }
+  })
+      .then(() => {
+        showNotify('Видео успешно загружено!', 'done');
+        isShowUploadModal.value = false;
+
+        videoFile.value = null
+      })
+      .catch((error: any) => showNotify(error.message ?? 'Произошла ошибка при загрузке видео'))
+      .finally(() => componentLoading.value = false)
 }
 
 definePageMeta({
